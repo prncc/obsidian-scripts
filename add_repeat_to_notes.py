400e01cd40776ffb337274e2b526bc6c0bd859e4
Andre Perunicic <perunicic@gmail.com>
2024-12-01 00:38:47 -0500

    Use never as default, and otherwise get interval in weeks

--- add_repeat_to_notes.py
+++ add_repeat_to_notes.py
@@ -5,7 +5,6 @@ import fire
 import frontmatter
 from rich.console import Console
 from rich.prompt import Prompt
-from rich.prompt import IntPrompt
 
 from utils import read_note
 from utils import render_note
@@ -20,29 +19,14 @@ def maybe_add_repeat_to_note(
     return False
   note_name = os.path.basename(note_path)
   render_note(console, note.content, note_name)
-  choices = {
-      'd': 'every (d)ay',
-      'w': 'every (w)eek',
-      'm': 'every (m)onth',
-      'y': 'every (y)ear',
-      's': '(s)paced...',
-      'n': '(n)one',
-  }
-  displayed_choices = '/'.join(choices.values())
   choice = Prompt.ask(
-      ('Add repeat field to metadata? '
-       f'[bold purple]{displayed_choices}[/bold purple]'),
-      default='n',
-      choices=choices.keys(),
-      show_choices=False,
+      'Repeat frequency in weeks?',
+      default='never',
   )
-  if choice == 'n':
-    return False
-  if choice == 's':
-    frequency = IntPrompt.ask('How many months between spaced intervals')
-    repeat = f'spaced every {frequency} months'
+  if choice in ['never', 'n', '0']:
+    repeat = 'never'
   else:
-    repeat = choices[choice].replace('(', '').replace(')', '')
+    repeat = f'spaced every {choice} weeks'
   note.metadata['repeat'] = repeat
   with open(note_path, 'wt') as f:
     f.write(frontmatter.dumps(note))
@@ -55,9 +39,13 @@ def add_repeat(directory: str = '~/Obsidian/Vault/Box'):
   console = Console()
   modified = {True: 0, False: 0}
   for note_path in glob.glob(f'{directory}/**.md'):
-    modified[maybe_add_repeat_to_note(console, note_path)] += 1
-  console.print(f'Added repeat to {modified[True]} notes. '
-                f'Kept {modified[False]} notes.')
+    try:
+      modified[maybe_add_repeat_to_note(console, note_path)] += 1
+    except KeyboardInterrupt:
+      print()
+      break
+  console.print(f'Added the repeat field to {modified[True]} notes. '
+                f'Skipped {modified[False]} already repeating notes.')
 
 
 if __name__ == '__main__':
